c8b332ca04a96a81df01f542d529c613115e0893
yungblud <killi8n@gmail.com>
2024-08-31 16:33:13 +0900

    feat: added career data parsing from Prisma

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,21 @@
 import { Fragment } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { format } from 'date-fns'
 import notionInstance, { notionDatabaseIds } from '../../lib/notionInstance'
 import { getBlocks } from '../../lib/notion'
+import { database } from '../../lib/database'
 import { renderBlock } from '../../components/notion/renderer'
 import styles from '../../styles/post.module.css'
+
+const getCareerData = async () => {
+  const data = await database.career.findMany({
+    orderBy: {
+      startDate: 'desc',
+    },
+  })
+  return data
+}
 
 async function queryNotionResumePage() {
   const res = await notionInstance.databases.query({
@@ -14,6 +25,7 @@ async function queryNotionResumePage() {
 }
 
 export default async function ResumePage() {
+  const careerData = await getCareerData()
   const resumeResult = await queryNotionResumePage()
   const { results } = resumeResult
   const page = results.at(0)
@@ -34,6 +46,63 @@ export default async function ResumePage() {
             // @ts-ignore
             <Fragment key={block.id}>{renderBlock(block)}</Fragment>
           ))}
+          <div>
+            {careerData.map((item, index) => (
+              <div
+                key={item.company}
+                style={{ display: 'flex', marginBottom: '12px' }}
+              >
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <div
+                    style={{
+                      width: '30px',
+                      height: '30px',
+                      borderRadius: '50%',
+                      background: '#2D2D2D',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginLeft: 'auto',
+                      marginRight: 'auto',
+                    }}
+                  >
+                    <p style={{ textAlign: 'center', margin: 'unset' }}>
+                      {careerData.length - index}
+                    </p>
+                  </div>
+                  <div
+                    style={{
+                      flex: 1,
+                      width: '1px',
+                      background: '#2D2D2D',
+                      marginLeft: 'auto',
+                      marginRight: 'auto',
+                    }}
+                  ></div>
+                </div>
+                <div style={{ width: '100%', marginLeft: '12px' }}>
+                  <div style={{ marginBottom: '12px' }}>
+                    {format(item.startDate, 'yyyy-MM')}
+                  </div>
+                  <div
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                      padding: '12px',
+                      backgroundColor: '#fff',
+                      borderRadius: '8px',
+                      boxShadow:
+                        '0px 3px 6px rgba(0, 0, 0, 0.16), 0px 3px 6px rgba(0, 0, 0, 0.23)', // Material Shadow
+                      fontSize: '16px',
+                      background: '#2D2D2D',
+                    }}
+                  >
+                    {item.company}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
           <Link href="/" className={styles.back}>
             ← Go home
           </Link>
