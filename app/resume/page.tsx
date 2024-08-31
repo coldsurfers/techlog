1d6d2edc2391db91a7dd7d55b0eac2f77bf0a8bc
yungblud <killi8n@gmail.com>
2024-08-31 14:33:09 +0900

    feat: added resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,4 +1,10 @@
+import { Fragment } from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
 import notionInstance, { notionDatabaseIds } from '../../lib/notionInstance'
+import { getBlocks } from '../../lib/notion'
+import { renderBlock } from '../../components/notion/renderer'
+import styles from '../../styles/post.module.css'
 
 async function queryNotionResumePage() {
   const res = await notionInstance.databases.query({
@@ -8,7 +14,31 @@ async function queryNotionResumePage() {
 }
 
 export default async function ResumePage() {
-  const resume = await queryNotionResumePage()
-  console.log(resume)
-  return null
+  const resumeResult = await queryNotionResumePage()
+  const { results } = resumeResult
+  const page = results.at(0)
+  if (!page) {
+    return null
+  }
+  const blocks = await getBlocks(page.id)
+
+  return (
+    <div>
+      <Head>
+        {/* @ts-ignore */}
+        <title>{page.properties.Title?.title[0].plain_text}</title>
+      </Head>
+      <article className={styles.container}>
+        <section>
+          {blocks.map((block) => (
+            // @ts-ignore
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+          <Link href="/" className={styles.back}>
+            ← Go home
+          </Link>
+        </section>
+      </article>
+    </div>
+  )
 }
