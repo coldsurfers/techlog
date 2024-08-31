cb49e064d2f9cd060fa64dfb7a2c8c9fd47cc264
yungblud <killi8n@gmail.com>
2024-08-31 13:58:40 +0900

    fix: fixed type issue of notion

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,7 @@
 import Link from 'next/link'
 import Text from '../components/text'
 import styles from './index.module.css'
-import notionInstance, { notionDatabaseIds } from '../lib/notionInstance'
-
-async function queryNotionBlogTechArticles() {
-  const platformFilter = {
-    property: 'platform',
-    multi_select: {
-      contains: 'techlog',
-    },
-  }
-  const result = await notionInstance.databases.query({
-    database_id: notionDatabaseIds.blog,
-    sorts: [
-      {
-        timestamp: 'created_time',
-        direction: 'descending',
-      },
-    ],
-    filter: platformFilter,
-  })
-
-  const posts = result?.results?.map((post) => {
-    // @ts-ignore
-    const createdTime = new Date(post.created_time)
-    // @ts-ignore
-    const lastEditedTime = new Date(post.last_edited_time)
-    // @ts-ignore
-    const slug = post.properties?.Slug?.rich_text?.at(0)?.text.content
-    // @ts-ignore
-    const title = post.properties?.Name?.title
-    // @ts-ignore
-    const postStatus = post.properties.Status.status.name
-    return {
-      id: post.id,
-      createdTime,
-      lastEditedTime,
-      dateLocale: createdTime.toLocaleString('en-US', {
-        month: 'short',
-        day: '2-digit',
-        year: 'numeric',
-      }),
-      slug,
-      title,
-      status: postStatus,
-    }
-  })
-
-  return posts.filter((post) => post.status === 'Published')
-}
+import { queryNotionBlogTechArticles } from '../lib/utils'
 
 export default async function Page() {
   const posts = await queryNotionBlogTechArticles()
