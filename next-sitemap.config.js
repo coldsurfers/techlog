28be2798f6e6e30a59c34c3e1c16aacfe643e683
yungblud <killi8n@gmail.com>
2024-08-31 16:37:41 +0900

    feat: updated sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://example.com',
   generateRobotsTxt: true,
   sitemapSize: 1000,
+  exclude: ['/resume'],
   // optional
   robotsTxtOptions: {
     additionalSitemaps: [`${process.env.SITE_URL}/server-sitemap.xml`],
